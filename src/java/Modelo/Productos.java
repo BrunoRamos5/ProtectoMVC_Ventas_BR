b8fdc3eb78c2f679d35cc7000f77e516121aacac
Bruno Ramos <brunex0517@gmail.com>
2020-08-03 12:29:08 -0500

    Conexion, creacion de Imagen, Productos java

--- src/java/Modelo/Productos.java
+++ src/java/Modelo/Productos.java
@@ -11,4 +11,63 @@ package Modelo;
  */
 public class Productos {
     
+    private int idproducto;
+    private String nombre;
+    private String codigo;
+    private float precio;
+    private int stock;
+    
+    public Productos(){
+        
+    }
+    
+    public Productos(String nombre, String codigo, float precio, int stock ){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.stock = stock;
+        
+    }
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
 }
